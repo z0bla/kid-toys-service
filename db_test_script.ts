cb2afceb0cf469733eaf0134d9ae027604cdcc15
Goran Stevanovic <stevanovicgoran@proton.me>
2022-12-10 20:19:13 +0100

    Update script that populates test database

--- db_test_script.ts
+++ db_test_script.ts
@@ -3,7 +3,30 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  //const user = await prisma.user.deleteMany();
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        firstName: "Dalibor",
+        lastName: "Blazanovic",
+      },
+      {
+        firstName: "Dejan",
+        lastName: "Blazanovic",
+      },
+      {
+        firstName: "Nemanja",
+        lastName: "Vasic",
+      },
+      {
+        firstName: "Goran",
+        lastName: "Stevanovic",
+      },
+    ],
+  });
+  console.log(users);
+
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
 }
 
 main()
