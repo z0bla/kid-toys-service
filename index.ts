1f01af5ef1a231f4dfd3993824c798a23270e757
Goran Stevanovic <stevanovicgoran@proton.me>
2022-12-11 20:25:24 +0100

    Add script to check database connection

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import prisma from "./prisma";
 
 const app = express();
 const port = "5000";
+
+async function connectDb() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected");
+  } catch (err) {
+    console.log("Error connectig to database: ", err);
+  }
+}
+
+connectDb();
 
 app.get("/*", (req, res) => {
   res.end("Uspjelo Nukrec!");
