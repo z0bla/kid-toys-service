6e562e5b38f8b732b7687ed53fb17fe3c43e0538
Goran Stevanovic <stevanovicgoran@proton.me>
2023-01-27 16:52:08 +0100

    Update user schema for validation

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,15 +1,17 @@
 import Joi from "joi";
 
 const userSchema = Joi.object({
-  email: Joi.string().email().required(),
   firstName: Joi.string().max(45),
   lastName: Joi.string().max(45),
   phoneNumber: Joi.number(),
+  address: Joi.string(),
+  email: Joi.string().email().required(),
   password: Joi.string()
     .regex(
       /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/
     )
     .required(),
+  role: Joi.string(),
 });
 
 export default userSchema;
