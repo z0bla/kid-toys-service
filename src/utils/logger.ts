2d02b639dc0f9e85ed2602147c0bb367991d50fb
Goran Stevanovic <stevanovicgoran@proton.me>
2023-02-08 16:33:56 +0100

    Restore original implementation using Winston and Logtail

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,32 +1,36 @@
-// const winston = require("winston");
-// const { combine, timestamp, json, errors } = winston.format;
-// const { Logtail } = require("@logtail/node");
-// const { LogtailTransport } = require("@logtail/winston");
+const winston = require("winston");
+const { combine, timestamp, json, errors } = winston.format;
+const { Logtail } = require("@logtail/node");
+const { LogtailTransport } = require("@logtail/winston");
 
-// const logtail = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN);
+console.log("logger.ts file");
+console.log(process.env.DATABASE_URL);
+console.log("\n\n\n");
 
-// const destinations =
-//   process.env.NODE_ENV === "production"
-//     ? [new winston.transports.Console(), new LogtailTransport(logtail)]
-//     : [new winston.transports.Console()];
+const logtail = new Logtail("fLhHrFNbdBe9DFFqSyp9RZZk");
 
-// export default winston.createLogger({
-//   level: process.env.WINSTON_LOG_LEVEL || "info",
-//   // optional: if we use same Logtail for both frontend and backend
-//   defaultMeta: {
-//     service: "kid-toys-service",
+const destinations =
+  process.env.NODE_ENV === "production"
+    ? [new winston.transports.Console(), new LogtailTransport(logtail)]
+    : [new winston.transports.Console()];
+
+export default winston.createLogger({
+  level: process.env.WINSTON_LOG_LEVEL || "info",
+  // optional: if we use same Logtail for both frontend and backend
+  defaultMeta: {
+    service: "kid-toys-service",
+  },
+  format: combine(errors({ stack: true }), timestamp(), json()),
+  transports: destinations,
+  exceptionHandlers: destinations,
+  rejectionHandlers: destinations,
+});
+
+// export default {
+//   info: (message: String) => {
+//     console.info(message);
 //   },
-//   format: combine(errors({ stack: true }), timestamp(), json()),
-//   transports: destinations,
-//   exceptionHandlers: destinations,
-//   rejectionHandlers: destinations,
-// });
-
-export default {
-  info: (message: String) => {
-    console.info(message);
-  },
-  error: (message: String) => {
-    console.error(message);
-  },
-};
+//   error: (message: String) => {
+//     console.error(message);
+//   },
+// };
